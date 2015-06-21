374d9dbcb0ed2c151cd049860f9f6ad501bcef87
Jesus Gollonet <jesusgollonet@gmail.com>
2015-06-21 15:13:31 +0200

    Implement basic steps to start a pen from a template
    First implemented html

--- localpen.sh
+++ localpen.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
-main(){
-    mkdir css js
-    touch index.html css/main.css js/main.js
+# run with source so you cd in your current session instead of a subshell
+# copy template
+# cd into directory
+# launch servers
+# open vim
+
+create_dirname(){
+    local prefix=htmlpen
+    local suffix=$(date +%s)  # "+%s" seconds from unix 
+    local dirname=$prefix"_"$suffix
+    echo $dirname
 }
+
+copy_html_template_to(){
+    echo 'copying html template'
+    echo $dirname
+    cp -r templates/html/ $1
+}
+
+cd_to(){
+    echo 'cding to '
+    ls $1
+    echo $1
+    cd $1 
+}
+open_vi(){
+    vi .
+}
+
+main(){
+   echo 'creating pen' 
+   local dirname=$(create_dirname)
+   copy_html_template_to $dirname
+   cd_to $dirname
+   open_vi
+   echo "dirname ____  $dirname"
+   #copyTemplate
+}
+
 
 main
